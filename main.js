f7f6ffbbdcd9c91a0d7324cc77ee7fd6898d2f5f
supersam710 <spillers.sam@gmail.com>
2021-01-30 14:49:56 -0800

    Testing object creation from json

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ var gameEngine = new GameEngine();
 var ASSET_MANAGER = new AssetManager();
 
 
+function tempCreateObject(jsonString) {
+	var tile = JSON.parse(jsonString);
+	console.log(tile);
+	console.log(tile instanceof Tile);
+	console.log(tile instanceof Start);
+}
+
 ASSET_MANAGER.downloadAll(function () {
 	var canvas = document.getElementById('gameWorld');
 	var ctx = canvas.getContext('2d');
@@ -15,7 +22,7 @@ ASSET_MANAGER.downloadAll(function () {
 	var start = new Start(gameEngine);
 
 	// download(start, 'tile.json', 'text/plain');
-	readTextFile("./tile.json", console.log)
+	readTextFile("./tile.json", tempCreateObject)
 
 
 });
