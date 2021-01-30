c0409113e84684f53f20e6dfd7cc857300371161
supersam710 <spillers.sam@gmail.com>
2021-01-30 14:37:42 -0800

    Initial commit

---
+++ loadsave.js
@@ -0,0 +1,23 @@
+function download(content, fileName, contentType) {
+    var a = document.createElement("a");
+    var file = new Blob([JSON.stringify(content, null, 2)], {type: contentType});
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+}
+
+function load(filename) {
+    var json = require(filename); //(with path)
+    console.log(json)
+}
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
---
+++ main.js
@@ -0,0 +1,21 @@
+var gameEngine = new GameEngine();
+
+var ASSET_MANAGER = new AssetManager();
+
+
+ASSET_MANAGER.downloadAll(function () {
+	var canvas = document.getElementById('gameWorld');
+	var ctx = canvas.getContext('2d');
+
+	gameEngine.init(ctx);
+
+	gameEngine.start();
+
+
+	var start = new Start(gameEngine);
+
+	// download(start, 'tile.json', 'text/plain');
+	readTextFile("./tile.json", null)
+
+
+});
